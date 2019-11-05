8ddba2ae88b5f0a1c8d5f7b9b672492ab2957341
Ruben Molenaar <rubenmolenaar2002@gmail.com>
2019-11-05 11:46:49 +0100

    blin

---
+++ labopdracht1.js
@@ -0,0 +1,14 @@
+function helloworld(number){
+	for (var i = 1; i <= number; i++) {
+		document.write("hello world");
+		document.write("<br>");
+	}
+}
+
+helloworld(3);
+
+document.write("<br>");
+document.write("<br>");
+
+helloworld(7);
+
